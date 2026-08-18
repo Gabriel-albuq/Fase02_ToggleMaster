8c7cfa223f0f06d66ef53c3c115d5d7e71f383e6
Gabriel <gabriel.albuquerque05@gmail.com>
2026-08-18 16:44:23 -0300

    final commit local + k8s

--- analytics-service/app.py
+++ analytics-service/app.py
@@ -21,6 +21,7 @@ load_dotenv()
 AWS_REGION = os.getenv("AWS_REGION")
 SQS_QUEUE_URL = os.getenv("AWS_SQS_URL")
 DYNAMODB_TABLE_NAME = os.getenv("AWS_DYNAMODB_TABLE")
+DYNAMODB_ENDPOINT_URL = os.getenv("AWS_DYNAMODB_ENDPOINT_URL") # Adicionado para o dynamodb local
 
 if not all([AWS_REGION, SQS_QUEUE_URL, DYNAMODB_TABLE_NAME]):
     log.critical("Erro: AWS_REGION, AWS_SQS_URL, e AWS_DYNAMODB_TABLE devem ser definidos.")
@@ -31,8 +32,11 @@ if not all([AWS_REGION, SQS_QUEUE_URL, DYNAMODB_TABLE_NAME]):
 try:
     session = boto3.Session(region_name=AWS_REGION)
     sqs_client = session.client("sqs")
-    dynamodb_client = session.client("dynamodb")
-    log.info(f"Clientes Boto3 inicializados na região {AWS_REGION}")
+    dynamodb_client = session.client("dynamodb", endpoint_url=DYNAMODB_ENDPOINT_URL)
+    log.info(
+        f"Clientes Boto3 inicializados na região {AWS_REGION}"
+        + (f" {DYNAMODB_ENDPOINT_URL}" if DYNAMODB_ENDPOINT_URL else "")
+    )
 except NoCredentialsError:
     log.critical("Credenciais da AWS não encontradas. Verifique seu ambiente.")
     sys.exit(1)
